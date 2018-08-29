3989bba9fdfc5de749e45c091aa495cc79ac4d19
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-08-29 14:52:15 -0500

    add RESTful API for experimentID

--- server/routes.js
+++ server/routes.js
@@ -4,11 +4,24 @@ const db = require('../database/index.js');
 const router = express.Router();
 
 
-//REST for create, retreive, modify, delete and experiment
-router.get('/', (req, res, next) => {
+//REST for create, retreive, modify, delete an experiment
+router.get('/:experimentID', (req, res, next) => {
 
 });
 
+router.post('/:experimentID', (req, res, next) => {
+
+});
+
+router.put('/:experimentID', (req, res, next) => {
+
+});
+
+router.delete('/:experimentID', (req, res, next) => {
+
+});
+
+//MAY NEED ANOTHER ROUTER FOR THESE????
 //train, test, predict
 router.post('/train', (req, res, next) => {
   //opens up a data file and creates modle
